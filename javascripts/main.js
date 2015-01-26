7bc34665d551b295a014d5464111866943cb467b
jefvel <geekku@gmail.com>
2015-01-26 17:38:49 +0100

    auto commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,10 @@
-console.log('This would be the main JS file.');
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+
+document.write(
+    unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E")
+);
+
+try {
+    var pageTracker = _gat._getTracker("UA-58949040-1");
+    pageTracker._trackPageview();
+} catch(err) {}
