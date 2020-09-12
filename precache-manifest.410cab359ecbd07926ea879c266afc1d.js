bca069d790af9a11efe75e338901d8e0ce0eaa4f
jefvel <akselkornesjo@gmail.com>
2020-09-12 17:36:45 +0200

    Updates

---
+++ precache-manifest.410cab359ecbd07926ea879c266afc1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102cc537de27decc0b7f1a119046b495",
+    "url": "/jefvel.github.io/index.html"
+  },
+  {
+    "revision": "e724a656de22c027bbea",
+    "url": "/jefvel.github.io/static/css/main.54a3f16c.chunk.css"
+  },
+  {
+    "revision": "9fbb1ad7df1069999cd1",
+    "url": "/jefvel.github.io/static/js/2.27c26303.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jefvel.github.io/static/js/2.27c26303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e724a656de22c027bbea",
+    "url": "/jefvel.github.io/static/js/main.0db5de2c.chunk.js"
+  },
+  {
+    "revision": "eb2900087f5de162730d",
+    "url": "/jefvel.github.io/static/js/runtime-main.56a130d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jefvel.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
